0911c38d17c3c35aa4619c1e2dc1379a5a9efcd3
Allecsx <alecss28@yahoo.com>
2025-02-25 11:06:03 +0000

    Basic JavaScript Projects Assignment
    1. Utilize window.alert() & document.write().
    2. Create and concatenate 2 variables.
    3.Write an expression and comments.
    4.Utilize script element and src attribute to link my js file.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+var Sent1 = "\'This is the beginning of the string";
+var Sent2 = " | and this is the end of the string.\'";
+// Declare two string variables
+
+var A = Sent1 + Sent2;
+// Concatenate two strings
+
+document.write(A);
+//Write the concatenated sentence in the document
+
+var B = 1 + 1;
+//Example of an expression
+
+window.alert(B);
+//Display the result of the expression
